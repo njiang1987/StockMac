841a0423d29c73b870fcdfca86f7cd093fbfcab4
njiang1987 <njiang1987@gmail.com>
2014-12-20 20:19:16 +0800

    Initial Commit

---
+++ StockMac/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  StockMac
+//
+//  Created by jn11585852 on 14/12/20.
+//  Copyright (c) 2014年 JiangNan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ StockMac/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  StockMac
+//
+//  Created by jn11585852 on 14/12/20.
+//  Copyright (c) 2014年 JiangNan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
